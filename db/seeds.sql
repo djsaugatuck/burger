dfe2d84bc6eb54f6b33702ae490c03911b48f435
djsaugatuck <derejohn@cisco.com>
2018-01-19 23:08:55 -0500

    homework13v1

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('5x Baconator Tripple Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chilli Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Nothing but Mayo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt Burger', true);
